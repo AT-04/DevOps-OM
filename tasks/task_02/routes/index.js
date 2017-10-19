3dfad80e0a8ea9d6025f58d965eb631332ec7c44
OvidioMiranda <ovidio.miranda@fundacion-jala.org>
2017-10-18 22:26:20 -0400

    Task_02-Create the first test

--- tasks/task_02/routes/index.js
+++ tasks/task_02/routes/index.js
@@ -1,9 +1,13 @@
+"use strict"
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    //res.render('index', { title: 'Express' });
+    res
+        .status(200)
+        .json({message: 'Hello World'})
 });
 
 module.exports = router;
---
+++ tasks/task_02/test/index.js
@@ -0,0 +1,23 @@
+"use strict"
+let request = require('supertest-as-promised');
+const api = require('../app')
+const host = api
+
+request = request(host)
+
+describe('Route Index, Hello World', function(){
+    describe('GET / ', function(){
+        it('I should return a hello world', function (done) {
+            request
+                .get('/')
+                .set('Accept','application/json')
+                .expect(200)
+                .expect('Content-Type',/application\/json/)
+                .end((err, res) => {
+                  let body = res.body
+                  expect(body).to.have.property('message','Hello World')
+                  done(err)
+            })
+        })
+    })
+})
